2e92d50d21af4aea2d00ce527191b0b2a2875cbd
Mambo Bryan <mambobryan@gmail.com>
2022-08-24 12:23:51 +0300

    step 1

--- app/src/main/java/com/example/gmailui/MainActivity.kt
+++ app/src/main/java/com/example/gmailui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gmailui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// TODO: set MainActivity content view with viewbinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
